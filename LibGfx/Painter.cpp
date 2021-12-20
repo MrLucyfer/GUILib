086b89f41eeb0c654ee7d1b041dc95b81ff7dcc6
Nil <nfolquer@gmail.com>
2021-12-20 15:32:33 +0100

    LibGfx: Core drawing functions

--- LibGfx/Painter.cpp
+++ LibGfx/Painter.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Painter.h"
+
+GFX::Painter::Painter(const GFX::Bitmap &bmp) {
+    m_bitmap = std::make_unique<Bitmap>(bmp);
+}
+
+void GFX::Painter::fillRect(const GFX::Rect &rect, const GFX::Color::NamedColor &color) {
+    Point start = rect.getLoc();
+    Size dim = rect.getSize();
+    Color c(color);
+
+    for(int y = start.y(); y < dim.height(); y++) {
+        for(int x = start.x(); x < dim.width(); x++) {
+            m_bitmap->setPixel(y, x, c.to32bit());
+        }
+    }
+
+}
+
+void GFX::Painter::drawLine(const GFX::Point &point, const GFX::Color::NamedColor &color) {
+
+}
--- LibGfx/Painter.h
+++ LibGfx/Painter.h
@@ -3,10 +3,21 @@
 //
 
 #pragma once
+#include "Bitmap.h"
+#include "Rect.h"
+#include "Color.h"
+#include <memory>
 
 namespace GFX {
 class Painter {
+public:
+    explicit Painter(const Bitmap& bmp);
+    ~Painter() = default;
 
+    void fillRect(const Rect& rect, const Color::NamedColor& color);
+    void drawLine(const Point& point, const Color::NamedColor& color);
+private:
+    std::unique_ptr<Bitmap> m_bitmap;
 };
 }
 

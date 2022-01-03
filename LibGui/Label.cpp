f08e8d377a6a0fc70213cf2471dc6058eee02b30
Nil <nfolquer@gmail.com>
2022-01-03 11:02:23 +0100

    LibGui: Adding Label
    Adding a foundation for a label widget

---
+++ LibGui/Label.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nil Folquer <nfolquer@gmail.com> on 31/12/2021.
+//
+
+#include "Label.h"
---
+++ LibGui/Label.h
@@ -0,0 +1,14 @@
+//
+// Created by Nil Folquer <nfolquer@gmail.com> on 31/12/2021.
+//
+
+#pragma once
+
+#include "Widget.h"
+namespace GUI {
+class Label : public Widget{
+public:
+private:
+};
+}
+
